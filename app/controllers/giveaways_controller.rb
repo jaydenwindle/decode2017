6ffe22fdc8c68388d36360bd29dcd4583346ffd0
Kirk Yuan <kirk12345123@gmail.com>
2017-03-31 19:58:13 -0400

    Working loop for giveaway printing ID and name

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -1,8 +1,13 @@
-class GiveawaysController < ApplicationController
+class GiveawaysController < ShopifyApp::AuthenticatedController
     def new
       products = ShopifyAPI::Product.find(:all, params: { limit: 250 })
+      @products = []
+      products.each do |product|
+        @products.push({id: product.id, handle: product.handle})
+      end
+      p @products
     end
-    
+
     def edit
     end
 
@@ -17,6 +22,5 @@ class GiveawaysController < ApplicationController
 
     def create
       storeID = ShopifyAPI::Shop.current.id
-      giveProducts =  
     end
 end
