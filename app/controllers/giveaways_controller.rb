67c5c1ee52495feff42de1d97c7fe0bf0bf9a6d9
Jayden Windle <jaydenwindle@gmail.com>
2017-03-31 14:05:35 -0400

    Added giveaway routes & views

---
+++ app/controllers/giveaways_controller.rb
@@ -0,0 +1,19 @@
+class GiveawaysController < ApplicationController
+    def new
+    end
+    
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def create
+    end
+end
---
+++ test/controllers/giveaways_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GiveawaysControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
