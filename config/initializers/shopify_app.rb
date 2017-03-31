686f66a028f75baf88a1db4e877a5b28a8516993
Jayden Windle <jaydenwindle@gmail.com>
2017-03-31 10:06:22 -0400

    Bootstrapped shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = ENV["SHOPIFY_API_KEY"]
+  config.secret = ENV["SHOPIFY_API_SECRET"]
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
