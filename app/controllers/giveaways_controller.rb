c431b7fa2252d6cce0fdb64bc467110900fa66dc
Jayden Windle <jaydenwindle@gmail.com>
2017-03-31 19:54:28 -0400

    added initial dashboard code

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -1,7 +1,10 @@
 class GiveawaysController < ShopifyApp::AuthenticatedController
     def index
         store_id = ShopifyAPI::Shop.current.id
+        puts store_id
+
         @giveaways = Giveaway.find_by(store: store_id)
+        puts @giveaways
     end
 
     def new
