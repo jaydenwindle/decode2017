520f71fd8b03aced81ca36d400ae75acf9b0d042
Akinyele Akintola-Febrissy@Chromebook <aakin013@uottawa.ca>
2017-03-31 17:43:19 -0400

    Git some **** done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
     root :to => 'home#index'
     mount ShopifyApp::Engine, at: '/'
-  
-    post '/users/create' => 'users#create'
 
+  	post '/users/create' => 'users#create'
 end
