169ced75d5df53b8ac10daadc1f0d9fb16f001be
Jayden Windle <jaydenwindle@gmail.com>
2017-03-31 18:58:59 -0400

    changed routes to giveaway

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
     resource :giveaways
 
-    root :to => 'home#index'
+    root :to => 'giveaways#index'
     mount ShopifyApp::Engine, at: '/'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
