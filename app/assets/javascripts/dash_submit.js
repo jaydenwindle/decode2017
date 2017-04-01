1fed2cca29be189401b87655faada0ddda4688bc
Karim Alibhai <karim@alibhai.co>
2017-04-01 16:57:29 -0400

    Rewrite all ES6 to ES5 and fix nav btn (#24)

--- app/assets/javascripts/dash_submit.js
+++ app/assets/javascripts/dash_submit.js
@@ -1,11 +1,11 @@
-~ async function ( CONFIG ) {
+~ function ( CONFIG ) {
   'use strict'
 
   // list of products
   var products = []
 
   // render view
-  var renderIf = () => {
+  var renderIf = function () {
     $('.prizesView')['fade' + ( products.length === 0 ? 'Out' : 'In' )](500)
 
     var btnSubmit = $('[type="submit"], #btnRmProduct')
@@ -14,27 +14,28 @@
     else if (products.length > 0 && btnSubmit.is('[disabled]')) btnSubmit.removeAttr('disabled')
   }
 
-  var renderProducts = () => {
-    products = products.filter(product =>
+  var renderProducts = function () {
+    products = products.filter(function (product) {
       product.gaQuantity > 0
-    )
+    })
 
-    $('#prizes').html(products.map((product, index) => (
-      `<li class="list-group-item justify-content-between" id="pid-${ index }">` +
-        `<div class="imgish p-4" style="background-image: url(${ (product.image || {}).src || '/assets/placeholder.jpg' }); "></div>` +
-        `${ product.title }` +
-        `<div class="justify-content-right">` +
-          `<span class="badge badge-default color-grey clickable click-minus">–</span>` +
-          `<span class="badge color-black quantity">${ product.gaQuantity }</span>` +
-          `<span class="badge badge-default color-grey clickable click-plus">+</span>` +
-          `<span class="badge color-grey clickable click-rm ml-2">x</span>` +
-        `</div>` +
-      `</li>`
-    )).join(''))
+    $('#prizes').html(products.map(function (product, index) {
+      return
+        '<li class="list-group-item justify-content-between" id="pid-${ index }">' +
+        '<div class="imgish p-4" style="background-image: url(' + ((product.image || {}).src || '/assets/placeholder.jpg') + '); "></div>' +
+        product.title +
+        '<div class="justify-content-right">' +
+          '<span class="badge badge-default color-grey clickable click-minus">–</span>' +
+          '<span class="badge color-black quantity">' + product.gaQuantity + '</span>' +
+          '<span class="badge badge-default color-grey clickable click-plus">+</span>' +
+          '<span class="badge color-grey clickable click-rm ml-2">x</span>' +
+        '</div>' +
+      '</li>'
+    })).join(''))
 
     var data = {}
 
-    products.forEach(product => {
+    products.forEach(function (product) {
       data[product.id] = product.gaQuantity
     })
 
@@ -61,70 +62,76 @@
     $('.click-rm').on('click', function (evt) {
       evt.preventDefault()
 
-      products = products.filter((_, index) =>
-       String(index) !== $(this).parents('.list-group-item').attr('id').substr(4)
-      )
+      products = products.filter(function (_, index) {
+       return String(index) !== $(this).parents('.list-group-item').attr('id').substr(4)
+      })
 
       render()
     })
   }
 
-  var render = () => requestAnimationFrame(() => {
-    // render products view
-    renderProducts()
+  var render = function () {
+    requestAnimationFrame(function () {
+      // render products view
+      renderProducts()
 
-    // render visibility of conditional view
-    renderIf()
-  })
-
-  // promisify product modal
-  var getProduct = () => new Promise((resolve, reject) => ShopifyApp.Modal.productPicker({
-    'selectMultiple': true
-  }, (success, data) => {
-    if (!success && (data && data.products && data.products.length === 0 || (typeof data === 'string' && data.indexOf('Modal closed') === 0))) {
-      success = true
-      data = { products: [] }
-    }
-
-    // crapsies with bad error handling
-    // TODO: don't do this
-    if (!success) {
-      return alert(JSON.stringify(data, null, 2))
-    }
-
-    // if all is good, just append
-    resolve(data.products.map(product => {
-      product.gaQuantity = 1
-      return product
-    }))
-  }))
-
-  // asyncify shopify
-  await new Promise((resolve) => ShopifyApp.ready(resolve))
-
-  // init the app (if not embeded)
-  if ( !document.getElementById('dash-script').getAttribute('data-embed') ) {
-    ShopifyApp.Bar.initialize(CONFIG)
+      // render visibility of conditional view
+      renderIf()
+    })
   }
 
-  // add binding for product modal
-  $('#btnAddProduct').on('click', async evt => {
-    evt.preventDefault()
+  // promisify product modal
+  var getProduct = function (resolve) {
+    ShopifyApp.Modal.productPicker({
+      'selectMultiple': true
+    }, function (success, data) {
+      if (!success && (data && data.products && data.products.length === 0 || (typeof data === 'string' && data.indexOf('Modal closed') === 0))) {
+        success = true
+        data = { products: [] }
+      }
 
-    products = products.concat(await getProduct())
-    render()
-  })
+      // crapsies with bad error handling
+      // TODO: don't do this
+      if (!success) {
+        return alert(JSON.stringify(data, null, 2))
+      }
 
-  // binding for quick removal
-  $('#btnRmProduct').on('click', evt => {
-    evt.preventDefault()
+      // if all is good, just append
+      resolve(data.products.map(function (product) {
+        product.gaQuantity = 1
+        return product
+      })
+    })
+  }
 
-    var target = $('#prizes').val()
-    products = products.filter(product =>
-      target.indexOf(String(product.id)) === -1
-    )
+  // asyncify shopify
+  ShopifyApp.ready(function () {
+    // init the app (if not embeded)
+    if ( !document.getElementById('dash-script').getAttribute('data-embed') ) {
+      ShopifyApp.Bar.initialize(CONFIG)
+    }
 
-    render()
+    // add binding for product modal
+    $('#btnAddProduct').on('click', function (evt) {
+      evt.preventDefault()
+
+      getProduct().then(function (prod) {
+        products = products.concat(prod)
+        render()
+      })
+    })
+
+    // binding for quick removal
+    $('#btnRmProduct').on('click', function (evt) {
+      evt.preventDefault()
+
+      var target = $('#prizes').val()
+      products = products.filter(function (product) {
+        return target.indexOf(String(product.id)) === -1
+      })
+
+      render()
+    })
   })
 }({
   title: document.title || 'Giveaways',
