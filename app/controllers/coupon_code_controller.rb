9f3d884f1588911e005339849b7c5a4a31bc4846
rahls7 <plaris.711@gmail.com>
2017-04-01 14:05:16 -0400

    Adding SHopify discount api

--- app/controllers/coupon_code_controller.rb
+++ app/controllers/coupon_code_controller.rb
@@ -1,7 +1,18 @@
 class CouponCodeController < ShopifyApp::AuthenticatedController
 
 	def create
-		
+		discount = ShopifyAPI::Discount.new(
+				{
+						discount_type: "percentage",
+						value: "100%",
+						code: ,
+						starts_at: Time.now,
+						ends_at: 5.days.from_now,
+
+				}
+		)
+
+
 	end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
+
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
   end
 end
