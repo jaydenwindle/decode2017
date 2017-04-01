b243bd5ab50edd5884c75b173e0a468269c8a30a
Jayden Windle <jaydenwindle@gmail.com>
2017-04-01 11:34:10 -0400

    added initial forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :giveaway
 
-	before_save {
-		u = User.find_by(email: self.email)
-		raise Exception.new("Sorry, this email #{self.email} has already been registered.") if u
-	}
-
 end
