7fba3c7cf4584edc0208972a3213a637a950b873
Jayden Windle <jaydenwindle@gmail.com>
2017-04-03 17:54:43 -0400

    Fixed winner choosing function, added winners list to giveaway model, added ability to list currently signed up users to show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401191616) do
+ActiveRecord::Schema.define(version: 20170403205717) do
 
   create_table "giveaways", force: :cascade do |t|
     t.integer  "store"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170401191616) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "giveaway_id"
+    t.string   "item_won"
     t.index ["giveaway_id"], name: "index_users_on_giveaway_id"
   end
 
