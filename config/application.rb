f625b9e786c6471f13174a1f6d237d1c22243511
Karim Alibhai <karim@alibhai.co>
2017-04-14 12:18:08 -0400

    Change use of coffeescript to JS

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,15 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Helloworld
   class Application < Rails::Application
     config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
     config.action_dispatch.default_headers.delete('X-Frame-Options')
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    
+    config.generators do |g|
+      g.javascript_engine :js
+    end
   end
 end
