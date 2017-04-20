3ba9c2663a1e8ebee60f3fe962df4f7bbd779153
Karim Alibhai <karim@alibhai.co>
2017-04-20 12:23:17 -0400

    Add CSS to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( dash_main.js )
 Rails.application.config.assets.precompile += %w( dash_submit.js )
+
+Rails.application.config.assets.precompile += %w( confirm.scss )
+Rails.application.config.assets.precompile += %w( entries.scss )
